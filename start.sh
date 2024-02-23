1848470dabc7f09bb79264fe31db684de622db92
mitinikitos <mityushkin.nik.99@gmail.com>
2024-02-23 12:35:09 +0300

    Сборка gradle в dockerfile + postgres volumes

--- start.sh
+++ start.sh
@@ -6,15 +6,6 @@ git pull
 # Checkout to needed git branch
 git checkout $1
 
-# Prepare JAR
-gradle clean
-gradle build
-rc=$?
-# if maven failed, then we will not deploy new version.
-if [ $rc -ne 0 ] ; then
-  echo Could not perform gradle clean install, exit code [$rc]; exit $rc
-fi
-
 # Add env vars to .env config file
 echo "$2" >> ./build/.env
 echo "$3" >> ./build/.env
