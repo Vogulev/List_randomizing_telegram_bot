ea7bb52684a462ae21068437efbb737e44356eb0
Artem Vogulev <art.vogulev@gmail.com>
2025-02-16 19:59:17 +0300

    add numbers for shuffle users list

--- src/main/java/com/vogulev/list_randomizing_telegram_bot/service/ShuffleService.java
+++ src/main/java/com/vogulev/list_randomizing_telegram_bot/service/ShuffleService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Service
@@ -13,10 +14,10 @@ import java.util.stream.Collectors;
 public class ShuffleService {
 
     public String shuffle(List<PbUser> users) {
+        var number = new AtomicInteger(1);
         Collections.shuffle(users);
         return users.stream()
-                .map(PbUser::getName)
-                .map(Object::toString)
+                .map(pbUser -> number.getAndIncrement() + ". " + pbUser.getName())
                 .collect(Collectors.joining("\n"));
     }
 }
